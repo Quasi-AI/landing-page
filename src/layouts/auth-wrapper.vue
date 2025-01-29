069ff95c29a0ae3578e3d3c9617864e10cdc7e00
danieladeabah <daniel.adeabah@amalitech.com>
2025-01-29 16:31:15 +0000

    Restructure pages and add layout wrappers

---
+++ src/layouts/auth-wrapper.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full bg-[#F1F3FE]">
+    <div class="container mx-auto flex h-screen items-center justify-center">
+      <slot></slot>
+    </div>
+  </div>
+</template>
---
+++ src/layouts/landing-wrapper.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="mx-auto w-full bg-[#F1F3FE]">
+    <slot></slot>
+  </div>
+</template>
